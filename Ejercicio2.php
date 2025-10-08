b598138a8a48707233420fdcf823a3ab538d4904
jucagolddev <jucagolddev@gmail.com>
2025-10-08 09:51:27 +0200

    Add files via upload

---
+++ Ejercicio2.php
@@ -0,0 +1,33 @@
+<?php
+/*1. Crea un archivo PHP que:
+○ Defina dos variables con valores numéricos y realice operaciones aritméticas
+básicas (suma, resta, multiplicación, división, módulo).
+○ Use operadores de comparación para verificar si una variable es mayor,
+menor o igual que la otra.
+○ Usa un operador ternario para determinar si una variable es par o impar.
+○ Aplica operadores lógicos para verificar si ambas variables son mayores de
+10 y muestra un mensaje dependiendo del resultado.
+○ Implementa operadores de incremento y decremento y muestra cómo
+cambian los valores de las variables.*/
+
+$numero1 =4;
+$numero2 =6;
+
+$suma = $numero1 +$numero2;
+$resta= $numero1 -$numero5;
+$mult= $numero1 * $numero1;
+$div= $numero1 / $numero1;
+
+echo $suma;
+echo"</br></br>";
+
+echo $resta;
+echo"</br></br>";
+
+echo $mult;
+echo"</br></br>";
+
+echo $div;
+echo"</br></br>";
+
+?>
